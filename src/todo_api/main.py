6c67b1d65653c8d2b7d500b97b3439b1c9fd130d
Siddharth Gupta <sid2harthgupta@gmail.com>
2025-08-09 08:21:47 -0700

    first commit

---
+++ src/todo_api/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI(title="Todo API", version="1.0.0")
+
+class Todo(BaseModel):
+    id: int
+    title: str
+
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id: int):
+    """Get a todo by ID"""
+    return {"id": todo_id, "title": "Sample"}
+
+@app.post("/todos")
+def create_todo(todo: Todo):
+    """Create a new todo"""
+    return todo
