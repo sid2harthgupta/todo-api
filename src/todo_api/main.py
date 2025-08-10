ea2e559b656c931c856399a6e1369def27577694
Siddharth Gupta <sid2harthgupta@gmail.com>
2025-08-09 19:22:49 -0700

    Introduce a delete endpoint

--- src/todo_api/main.py
+++ src/todo_api/main.py
@@ -9,10 +9,15 @@ class Todo(BaseModel):
 
 @app.get("/todos/{todo_id}")
 def get_todo(todo_id: int):
-    """Get a todo by ID"""
+    """Retrieve a to-do by its ID"""
     return {"id": todo_id, "title": "Test 7"}
 
 @app.post("/todos")
 def create_todo(todo: Todo):
-    """Create a new todo"""
+    """Create a new to-do"""
     return todo
+
+@app.delete("/todos/{todo_id}")
+def create_todo(todo_id: int):
+    """Delete a to-do using its ID"""
+    return None
