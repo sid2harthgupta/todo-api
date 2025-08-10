105776672db20897b23504c851a7745f7203ae61
Siddharth Gupta <sid2harthgupta@gmail.com>
2025-08-09 19:59:00 -0700

    Simplify yml file

--- src/todo_api/main.py
+++ src/todo_api/main.py
@@ -9,15 +9,15 @@ class Todo(BaseModel):
 
 @app.get("/todos/{todo_id}")
 def get_todo(todo_id: int):
-    """Retrieve a to-do by its ID"""
+    """Retrieve a to-do by its ID."""
     return {"id": todo_id, "title": "Test 7"}
 
 @app.post("/todos")
 def create_todo(todo: Todo):
-    """Create a new to-do"""
+    """Create a new to-do."""
     return todo
 
 @app.delete("/todos/{todo_id}")
 def create_todo(todo_id: int):
-    """Delete a to-do using its ID"""
+    """Delete a to-do using its ID."""
     return None
